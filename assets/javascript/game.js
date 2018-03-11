406960d663aafd020db6fd5cef892848382280a1
phodde12 <33789416+phodde12@users.noreply.github.com>
2018-03-10 19:17:06 -0600

    Add files via upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+var guessedLetters = [];
+
+var letterToGuess = null;
+ 
+var guessesLeft = 9;
+  
+var wins = 0;
+var losses = 0;
+  
+var updateGuessesLeft = function() {
+   
+    document.querySelector("#guesses-left").innerHTML = guessesLeft;
+  };
+var updateLetterToGuess = function() {
+    
+    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+  };
+var updateGuessesSoFar = function() {
+   
+    document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+  };
+  
+var reset = function() {
+    guessesLeft = 9;
+    guessedLetters = [];
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+  };
+  
+  updateLetterToGuess();
+  updateGuessesLeft();
+  
+  document.onkeyup = function(event) {
+    
+    guessesLeft--;
+    
+    var letter = String.fromCharCode(event.which).toLowerCase();
+    
+    guessedLetters.push(letter);
+    
+    updateGuessesLeft();
+    updateGuessesSoFar();
+    
+    if (letter === letterToGuess) {
+      
+      wins++;
+      document.querySelector("#wins").innerHTML = wins;
+     
+      reset();
+    }
+    
+    if (guessesLeft === 0) {
+      
+      losses++;
+      document.querySelector("#losses").innerHTML = losses;
+      
+      reset();
+    }
+  };
